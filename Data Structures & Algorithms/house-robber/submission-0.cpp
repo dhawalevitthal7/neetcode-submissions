f215220880ba1b8f558baa55eaf03bdc6c39422a
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-08-10 19:44:59 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int func(vector<int>& arr,int idx,int ans,vector<int>& dp){
+        if(idx<0) return ans;
+        // take
+        if(dp[idx]!= -1) return dp[idx];
+        return dp[idx] = ans+ max(func(arr,idx-1,ans,dp),arr[idx]+func(arr,idx-2,ans,dp));
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return func(nums,nums.size()-1,0,dp);
+    }
+};
