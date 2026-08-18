7f224aa5efb8224ae254ef18badfea1380c522d9
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-08-18 17:25:17 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            if(!st.empty() && ((s[i]=='}' && st.top()=='{') ||(s[i]==']' && st.top()=='[')|| (s[i]==')'&& st.top()=='('))) st.pop();
+            else st.push(s[i]);
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
